a4fe3c3333cf24dcee3616ce92379e48fe47dff8
NikhilUM <ndivekar@umich.edu>
2024-05-15 17:05:59 -0400

    .

--- ip_git_pusher.sh
+++ ip_git_pusher.sh
@@ -4,10 +4,7 @@ HOSTNAME=$(hostname -f)
 
 cd /home/pi/MBLUE-config
 git pull origin main
-
-python /home/pi/MBLUE/utilities/ip_yaml_editor.py
-
-cd /home/pi/MBLUE-config
+python ip_yaml_editor.py
 git add -A
 git commit -a -m "Updated IP address: $HOSTNAME"
 git push origin main
