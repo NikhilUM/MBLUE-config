63a3e8da372cb0f927cc8fe99ca7903ef75b4ae3
NikhilUM <ndivekar@umich.edu>
2024-05-15 17:10:06 -0400

    .

--- ip_yaml_editor.py
+++ ip_yaml_editor.py
@@ -21,7 +21,7 @@ def get_ip_address() -> str:
 
 if __name__ == "__main__":
 
-    with open("/home/pi/MBLUE/MBLUE-config/config.yaml", "r") as file:
+    with open("config.yaml", "r") as file:
         ip_addresses = yaml.safe_load(file)
 
     print("Existing IP addresses: ", ip_addresses)
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         elif "avani" in hostname.lower():
             ip_addresses["RPI_SYNC_IP"] = ip_address
 
-    with open("/home/pi/MBLUE/MBLUE-config/config.yaml", "w") as file:
+    with open("config.yaml", "w") as file:
         yaml.dump(ip_addresses, file)
 
     print("Updated IP addresses: ", ip_addresses)
